9f8ca98cc92bc6d4433473a111c8357264a417a6
Rose Stough <psithurise@programmer.net>
2019-04-10 23:58:55 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,20 +1,20 @@
 // declare your functions here...
 function paragraphSelector() {
-  $('p')
+  return $('p')
 }
 
 function lastImageSelector() {
-  $('img:last')
+  return $('img:last')
 }
 
 function ninjaBabySelector() {
-  $('#baby-ninja')
+  return $('#baby-ninja')
 }
 
 function divSelector() {
-  $(.pics)
+  return $(.pics)
 }
 
 function firstListItem() {
-  $('div ul:first-child')
+  return $('div ul:first-child')
 }
